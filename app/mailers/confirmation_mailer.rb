26765d24f3048b42cdff916a18e4d3fe709ab281
yuji91 <yuji91.1227@gmail.com>
2019-05-12 19:55:54 +0900

    refactor: findではなく@feedのAssociationを利用してuser.emailを取得

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -1,7 +1,6 @@
 class ConfirmationMailer < ApplicationMailer
   def confirmation_mail(feed)
     @feed = feed
-    @user = User.find(feed.user_id)
-    mail to: @user.email, subject: "投稿確認メール"
+    mail to: @feed.user.email, subject: "投稿確認メール"
   end
 end
