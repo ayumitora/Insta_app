2829bd7e5e3d1ca0704dce6ee43c1383b3e2dfd9
yuji91 <yuji91.1227@gmail.com>
2019-05-12 19:55:25 +0900

    refactor: @confirmationの廃止（@feedと同一の内容なので、@feedを使うべき）

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -1,7 +1,7 @@
 class ConfirmationMailer < ApplicationMailer
-  def confirmation_mail(confirmation)
-    @confirmation = confirmation
-    @user = User.find(confirmation.user_id)
+  def confirmation_mail(feed)
+    @feed = feed
+    @user = User.find(feed.user_id)
     mail to: @user.email, subject: "投稿確認メール"
   end
 end
